1b1fa0c6ae1100acd6e1df2f1a3caba17f2044d3
Bishesh Rana <rana.bishesh.5@gmail.com>
2021-08-23 20:39:38 +0545

    sign up

--- site/wow/advertisement/urls.py
+++ site/wow/advertisement/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name = 'index'),
+    path('sign',views.sign, name = 'sign'),
+    path('log',views.log, name = 'log')
 ]
--- site/wow/advertisement/views.py
+++ site/wow/advertisement/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
+
+def sign(request):
+
+    return render(request, 'sign.html')
+
+def log(request):
+    return render(request, 'login.html')
